db6ef5c98f31e148609996a3d5a1d3bc9c8e350b
Urs Waldmann <urs.waldmann@uni-konstanz.de>
2020-10-26 11:21:38 +0100

    Change and add subjects for training and testing in mvn/datasets/human36m.py
    train_subjects = ['S2', 'S3']
    test_subjects = ['S1']

--- mvn/datasets/human36m.py
+++ mvn/datasets/human36m.py
@@ -72,7 +72,7 @@ class Human36MMultiViewDataset(Dataset):
         n_cameras = len(self.labels['camera_names'])
         assert all(camera_idx in range(n_cameras) for camera_idx in self.ignore_cameras)
 
-        train_subjects = ['S1']
+        train_subjects = ['S2', 'S3']
         test_subjects = ['S1']
 
         train_subjects = list(self.labels['subject_names'].index(x) for x in train_subjects)

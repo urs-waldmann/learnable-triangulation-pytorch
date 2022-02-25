14695cc22755c25f076a2a3656b53dd2bdd15ab4
Urs Waldmann <urs.waldmann@uni-konstanz.de>
2022-02-25 17:44:35 +0100

    Change from 17 to 33 keypoints
    Update mvn/datasets/human36m.py
    Update mvn/utils/vis.py

--- mvn/datasets/human36m.py
+++ mvn/datasets/human36m.py
@@ -98,8 +98,8 @@ class Human36MMultiViewDataset(Dataset):
 
         self.labels['table'] = self.labels['table'][np.concatenate(indices)]
 
-        self.num_keypoints = 16 if kind == "mpii" else 17
-        assert self.labels['table']['keypoints'].shape[1] == 17, "Use a newer 'labels' file"
+        self.num_keypoints = 16 if kind == "mpii" else 33
+        assert self.labels['table']['keypoints'].shape[1] == 33, "Use a newer 'labels' file"
 
         self.keypoints_3d_pred = None
         if pred_results_path is not None:
@@ -252,10 +252,10 @@ class Human36MMultiViewDataset(Dataset):
             keypoints_gt = keypoints_gt[:, human36m_joints]
             keypoints_3d_predicted = keypoints_3d_predicted[:, cmu_joints]
 
-        # mean error per 16/17 joints in mm, for each pose
+        # mean error per 16/33 joints in mm, for each pose
         per_pose_error = np.sqrt(((keypoints_gt - keypoints_3d_predicted) ** 2).sum(2)).mean(1)
 
-        # relative mean error per 16/17 joints in mm, for each pose
+        # relative mean error per 16/33 joints in mm, for each pose
         if not (transfer_cmu_to_human36m or transfer_human36m_to_human36m):
             root_index = 6 if self.kind == "mpii" else 6
         else:
